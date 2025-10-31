e46d976f937989ba86d431cf625b61485ecc0908
sagar <sagarpal786t@gmail.com>
2025-11-01 00:18:03 +0530

    User and Account Model created

---
+++ src/main/java/com/onefeed/userservice/models/User.java
@@ -0,0 +1,36 @@
+package com.onefeed.userservice.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "full_name", nullable = false)
+    private String full_name;
+
+    @Column(name = "password", nullable = false, length = 255)
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<Account> accounts;
+
+    // improvements : need to add created_at and updated_at
+    /*
+    addAccount(Account account){
+        accounts.add(account);
+    }
+     */
+}
